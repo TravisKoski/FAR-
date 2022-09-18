3281149f8d38f0b1941678308f690a122e020111
Travis Koski <tmkoski69@gmail.com>
2022-09-17 18:19:53 -0700

    Inventory audit page allows us to set the view category, and it reloads everytime that state changes

--- far-gui/src/components/inventory_audit/inventory_list.js
+++ far-gui/src/components/inventory_audit/inventory_list.js
@@ -1,6 +1,15 @@
-const ItemList = (props) =>{
+const CategoryFilter = ({filter,setFilter}) =>{
+    const onSelect = (e) => {setFilter(e.target.value)};
     return (
-        <h1>this is where the item list will be displayed</h1>
+        <div>
+        <label>Select a category of items to view (dry grocery, frozen, etc..)</label>
+        <select value = {filter} onChange = {onSelect}>
+            <option value = "dry grocery"> Dry grocery</option>
+            <option value = "frozen"> Frozen foods</option>
+            <option value = "perishable">Dairy/refrigerated</option>
+
+        </select>
+        </div>
     )
 };
-export default ItemList
+export default CategoryFilter
--- far-gui/src/pages/inventory_audit.js
+++ far-gui/src/pages/inventory_audit.js
@@ -1,12 +1,13 @@
-import ItemList from "../components/inventory_audit/inventory_list"
+import CategoryFilter from "../components/inventory_audit/inventory_list"
 import ApiService from "../utils/fetch";
 import {useState, useEffect} from "react"
 const ItemAudit = () =>{
     const[items, setItems] = useState([]);
+    const [categoryToView, setCategoryToView] = useState("");
     //below function calls the index method from backend
     const fetchItems = () =>{
         const fetcher = ApiService();
-        const getAllItemsUrl = "http://localhost:3000/items";
+        const getAllItemsUrl = `http://localhost:3000/items/${categoryToView}`;
         fetcher.get(getAllItemsUrl)
         .then(response => {setItems(response.data); console.log(response.data)})
         .catch(error =>{console.log(error)});
@@ -14,10 +15,13 @@ const ItemAudit = () =>{
     }
     useEffect(()=>{
         fetchItems();
-        console.log(items)
-    },[])
+        console.log(items);
+    },[categoryToView])
     return(
-    <ItemList/>
+        <>
+    <CategoryFilter filter = {categoryToView} setFilter = {setCategoryToView}/>
+    <></>
+    </>
     )
 };
 export default ItemAudit;
