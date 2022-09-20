b6ea63181f5f9c05ef4bc2b44bb4118cbcb2344d
Travis Koski <tmkoski69@gmail.com>
2022-09-20 16:12:12 -0700

    Added controller stubs for order batches

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,16 @@
+class OrdersController < ApplicationController
+  def open
+  end
+
+  def add_item
+  end
+
+  def review
+  end
+
+  def close
+  end
+
+  def transmit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  post 'orders/open', to: "orders#open"
+  put 'orders/add_item', to: "orders#add_item"
+  get 'orders/review', to: "orders#review"
+  delete 'orders/close', to: "orders#close"
+  get 'orders/transmit', to: "orders#transmit"
+
   get "/items/find_by_category/:category", to: "items#by_category"
   put "/items/:id/adjust_PI/", to: "items#update_PI"
   resources :items
