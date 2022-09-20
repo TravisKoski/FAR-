cd2b2bc46b3661073edda69e02a004e937221113
Travis Koski <tmkoski69@gmail.com>
2022-09-20 16:32:31 -0700

    Realized I didnt need an open action, also saving work on add item logic with some pseudocode

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,13 @@
 class OrdersController < ApplicationController
-  def open
-  end
 
   def add_item
+    item_to_add = Item.find_by_id(params[:item_id])
+    number_of_cases = params[:case_count].to_i
+    #TODO:
+    #1. Check if an order is already open under the item category and
+    #2: If Not, open one with the correct category
+    #3. See if the item has already been ordered by seeing if the order tiket has already been made
+    #4. IF yes, update it. If not, create the ticket and add it to the order
   end
 
   def review
