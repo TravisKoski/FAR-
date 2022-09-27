1d2da99f4dd6605faa7e046698098da94938d288
Travis Koski <tmkoski69@gmail.com>
2022-09-27 12:34:05 -0700

    Fixed a bug where an item was going into an order ticket with a null quantity

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,7 +12,7 @@ class OrdersController < ApplicationController
       if order_ticket
         order_ticket.update!(:quantity=>number_of_cases)
       else
-        order_ticket = OrderTicket.create(:item_id=>item_to_add.id, :order_batch_id=>active_order.id,
+        order_ticket = OrderTicket.create!(:item_id=>item_to_add.id, :order_batch_id=>active_order.id,
         :quantity=>number_of_cases)
       end
       render json: active_order.to_json(:include=>:order_tickets) and return
@@ -20,7 +20,7 @@ class OrdersController < ApplicationController
       #we know that it is the first time ordering this item
       #A new order must be made, and a new order ticket
     new_order = OrderBatch.create!(:category=>item_to_add.category, :from=>"SOLO", :active=>true)
-    new_ticket = OrderTicket.create!(:item_id=>item_to_add.id, :order_batch_id=>new_order.id)
+    new_ticket = OrderTicket.create!(:item_id=>item_to_add.id, :order_batch_id=>new_order.id, :quantity=>number_of_cases)
     render json: new_order.to_json(:include=>:order_tickets)
     #TODO, could clean up this code a bit, tb
   end
